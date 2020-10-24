00e6e74d74a8341605315ef731a26951f3da19c6
Alexander Ignition <izh.sever@gmail.com>
2020-10-24 22:24:47 +0300

    Added xc-gh

--- Tests/GHActionTests/GHActionTests.swift
+++ Tests/GHActionTests/GHActionTests.swift
@@ -1,28 +1,10 @@
 import XCTest
-import Logging
-import LoggingGitHubActions
 @testable import GHAction
+
+#warning("Something went wrong")
 
 final class GHActionTests: XCTestCase {
     func testExample() {
-
-        LoggingSystem.bootstrap { label in
-            if ProcessInfo.processInfo.environment["GITHUB_ACTIONS"] == "true" {
-                return GitHubActionsLogHandler.standardOutput(label: label)
-            } else {
-                return StreamLogHandler.standardOutput(label: label)
-            }
-        }
-
-        XCTAssertEqual(GHAction().text, "Hello, World!")
-
-        let logger = Logger(label: "com.example.MyApp")
-        logger.trace("trace")
-        logger.debug("debug")
-        logger.info("info")
-        logger.notice("notice")
-        logger.warning("warning")
-        logger.error("Something went wrong")
-        logger.critical("critical")
+        XCTAssertEqual(GHAction().text, "Hello, World")
     }
 }
